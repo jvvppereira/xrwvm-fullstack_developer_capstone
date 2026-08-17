a2d4e355a2642f39a6c8b8d1fc942bbff1f9249f
João Vítor <joao.vitor.paes@hotmail.com>
2026-08-18 01:51:07 +0200

    Specify exception type in error handling

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -66,7 +66,7 @@ def searchcars_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
     finally:
